e2b41ebed7db367af60f6ab6e01873837cc3c1d7
Persis Ratouis <pratouis@users.noreply.github.com>
2018-10-05 00:44:05 -0500

    removing commented line

--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,6 @@ module.exports = {
           {loader: "css-loader"},
           {loader: "less-loader",
             options: {
-              // modifyVars: themeVars,
               javascriptEnabled: true
             }
           }
